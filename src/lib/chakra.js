6e0d5abd63edb767e17995b728237d32d05d7829
yagiz2000 <yagzceritoglu@gmail.com>
2023-01-23 10:54:53 +0300

    feat: chakra custom theme config file system built

---
+++ src/lib/chakra.js
@@ -0,0 +1,19 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const components = {
+  Heading: {
+    variants: {
+      "section-title": {
+        textDecoration: "underline",
+        fontSize: 20,
+        textUnderlineOffset: 6,
+        textDecorationColor: "#525252",
+        textDecorationThickness: 4,
+        marginTop: 3,
+        marginBottom: 4,
+      },
+    },
+  },
+};
+
+export const theme = extendTheme({ components });
