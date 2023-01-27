1e07ea76a036ced9747d91e3779c6ce2b9c0e809
yagiz2000 <yagzceritoglu@gmail.com>
2023-01-27 18:18:55 +0300

    feat: logo component implementation

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Head from "next/head";
 import NextLink from "next/link";
 import {
@@ -12,6 +13,7 @@ import {
 import ThemeTogglerButton from "@/components/theme-toggler-button";
 import MobileMenu from "@/components/mobile-menu";
 import FlagMenu from "./flag-menu";
+import Logo from "./logo";
 
 export default function Navbar() {
   return (
@@ -23,40 +25,38 @@ export default function Navbar() {
         <meta charset="UTF-8"></meta>
         <link rel="icon" href="/favicon.ico?v=2" type="image/x-icon" />
       </Head>
-      <main>
-        <Box borderRadius="lg" mb={6} p={3}>
-          <Container
-            display="flex"
-            justifyContent="space-between"
-            bg="green.800"
-            color="white"
-            borderRadius="lg"
-            alignItems="center"
-            maxW="container.md"
-            p={2}
-          >
-            <Text fontWeight="bold">Yağız Ceritoğlu</Text>
-            <Stack direction="row" alignItems="center">
-              <Stack
-                display={{ base: "none", md: "flex" }}
-                direction="row"
-                alignItems="center"
-              >
-                <Link as={NextLink} href="#">
-                  Projects
-                </Link>
-                <Link as={NextLink} href="#">
-                  Expriences
-                </Link>
-              </Stack>
-
-              <ThemeTogglerButton></ThemeTogglerButton>
-              <MobileMenu></MobileMenu>
-              <FlagMenu></FlagMenu>
+      <Box borderRadius="lg" mb={6} p={3}>
+        <Container
+          display="flex"
+          justifyContent="space-between"
+          bg="green.800"
+          color="white"
+          borderRadius="lg"
+          alignItems="center"
+          maxW="container.md"
+          p={2}
+        >
+          <Logo />
+          <Stack direction="row" alignItems="center">
+            <Stack
+              display={{ base: "none", md: "flex" }}
+              direction="row"
+              alignItems="center"
+            >
+              <Link as={NextLink} href="/projects">
+                Projects
+              </Link>
+              <Link as={NextLink} href="#">
+                Expriences
+              </Link>
             </Stack>
-          </Container>
-        </Box>
-      </main>
+
+            <ThemeTogglerButton />
+            <MobileMenu />
+            <FlagMenu />
+          </Stack>
+        </Container>
+      </Box>
     </>
   );
 }
