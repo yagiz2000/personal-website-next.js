c358cecc2cabbe3b01cfc20cfff3dd2e65c8ee0a
yagiz2000 <yagzceritoglu@gmail.com>
2023-01-27 18:20:17 +0300

    feat: projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,41 @@
+import {
+  Heading,
+  Box,
+  Container,
+  Stack,
+  UnorderedList,
+  ListItem,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Layout from "@/layout";
+import ProjectCard from "@/components/project-card";
+
+export default function Experiences() {
+  const projects = [
+    { name: "Microsoft Todo List Clone" },
+    { name: "Social Media App" },
+    { name: "Type Script Todo" },
+    { name: "Gallery Vue" },
+    { name: "sa" },
+    { name: "tes" },
+  ];
+  return (
+    <>
+      <Layout>
+        <Container>
+          <Heading mb={5}>Projects</Heading>
+          <Stack
+            direction="row"
+            justifyContent="space-evenly"
+            wrap="wrap"
+            rowGap={5}
+          >
+            {projects.map((project, i) => (
+              <ProjectCard key={i} project={project} />
+            ))}
+          </Stack>
+        </Container>
+      </Layout>
+    </>
+  );
+}
