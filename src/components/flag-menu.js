b862fdffad518fd6e20c55c9c3b511a76ef9873a
yagiz2000 <yagzceritoglu@gmail.com>
2023-02-04 15:02:42 +0300

    feat: flag menu component

--- src/components/flag-menu.js
+++ src/components/flag-menu.js
@@ -1,16 +1,16 @@
-import Image from "next/image";
-import { Box, Button, Stack } from "@chakra-ui/react";
 import { useState } from "react";
+import { Box, Button, Stack } from "@chakra-ui/react";
 
 export default function FlagMenu() {
   const [flag, setFlag] = useState("Tr");
+
   return (
     <>
       <Stack direction="row">
-        <Button variant="outline" fontSize="30">
+        <Button className="flag-button" fontSize="25">
           🇹🇷
         </Button>
-        <Button variant="outline" fontSize="30">
+        <Button className="flag-button" fontSize="25">
           🇬🇧
         </Button>
       </Stack>
