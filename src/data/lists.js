166bd44c91d123fff34223ff2b58a6a855159d8c
yagiz2000 <yagzceritoglu@gmail.com>
2023-03-22 16:55:11 +0300

    feat: data folder, lists, texts files

---
+++ src/data/lists.js
@@ -0,0 +1,30 @@
+export const hobbiesList = [
+  "Travel 🧳",
+  "Read 📚",
+  "Listen 🎶 (also podcasts and people)",
+  "Build 🧑🏻‍💻 (software and systems)",
+  "Play 🎸 (begginer)",
+  "Cars and Motor Sports 🏎️ (Tifosi)",
+  "Exercise 🏃🏻 🏋🏻 🚴🏻‍♂️ 🏊🏻‍♂️",
+  "Video Games 🎮",
+];
+
+export const bioInfoList = [
+  { year: "2000", text: "Born in Izmir, Türkiye" },
+  {
+    year: "2018",
+    text: "Completed İzmir Anadolu Lisesi (High School Degree)",
+  },
+  {
+    year: "2018",
+    text: "Started ITU as a maritime engineering student",
+  },
+  {
+    year: "2020",
+    text: "Undergraduate transfer to physics engineering in ITU",
+  },
+  {
+    year: "2021",
+    text: "In September started working as Full Stack Developer at Gameflex",
+  },
+];
---
+++ src/data/texts.js
@@ -0,0 +1,2 @@
+export const summaryText =
+  "I am a final year physics engineering student and a full-stack developer based in Istanbul (Izmir), Türkiye. Since the beginning of 2021, I have been focusing on improving my skills in web development, particularly in the areas of JavaScript. With experience gained through internships, projects, and school teams, I landed a full-time job in October 2021.";
