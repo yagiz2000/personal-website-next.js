4692da8bfeb76f638b9ea723bbd01ffc5f032263
yagiz2000 <yagzceritoglu@gmail.com>
2023-03-22 18:01:53 +0300

    feat: footer data

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,22 @@
-import { useColorModeValue, Text } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useColorModeValue, Text, Link } from "@chakra-ui/react";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <>
-      <Text
-        my={7}
-        textAlign="center"
-        color={useColorModeValue("black", "white")}
-      >
-        {currentYear} Copyright Yağız Ceritoğlu
+      <Text my={7} textAlign="center">
+        {currentYear} Copyright Yağız Ceritoğlu,
+      </Text>
+      <Text my={2} textAlign="center">
+        Developed with{" "}
+        <Link as={NextLink} href="https://nextjs.org/">
+          Next
+        </Link>{" "}
+        and{" "}
+        <Link as={NextLink} href="https://chakra-ui.com/">
+          Chakra UI
+        </Link>
       </Text>
     </>
   );
