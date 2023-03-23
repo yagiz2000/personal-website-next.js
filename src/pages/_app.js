43ad596f085190876fc8edfa2f8c709d16649678
yagiz2000 <yagzceritoglu@gmail.com>
2023-03-23 16:23:52 +0300

    feat: vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
-import "@/styles/globals.scss";
+import { Analytics } from "@vercel/analytics/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@/lib/chakra";
+import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
