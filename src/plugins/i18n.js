9efe9ae08e2d1635d4d35ad77be814210f6785bd
Yağız Ceritoğlu <yagizceritoglu@Yagz-MacBook-Pro.local>
2023-08-13 12:38:45 +0300

    feat: use static lists

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,14 +1,18 @@
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import enTranslations from '../locales/en.json'
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import enTranslations from "../locales/en.json";
+import trTranslations from "../locales/tr.json";
 
 i18n.use(initReactI18next).init({
-  lng: 'en',
+  lng: "en",
   resources: {
     en: {
-      translation: enTranslations
-    }
-  }
-})
+      translation: enTranslations,
+    },
+    tr: {
+      translation: trTranslations,
+    },
+  },
+});
 
-export default i18n
+export default i18n;
